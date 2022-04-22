fbb2e0562cd90183c931640a010e4158351dafc6
DanielekPark <54155014+DanielekPark@users.noreply.github.com>
2022-04-22 10:04:13 -0700

    added fetch request functionality

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
-const form = document.querySelector('#form');
+const form = document.querySelector('#form')
 const input = document.querySelector('.form-control')
 
 form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  // sessionStorage.setItem('inputVal', input.value); 
+  e.preventDefault()
+  fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+      value: input.value
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+  .then(response => response.json())
+  // Displaying results to console
+  .then(json => console.log(json));
 })
