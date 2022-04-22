9a57291ccfd05a49fe4a467c72f4ee56ff9ba3b1
DanielekPark <54155014+DanielekPark@users.noreply.github.com>
2022-04-22 09:51:32 -0700

    added javascript code

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
-console.log('hello');
+const form = document.querySelector('#form');
+const input = document.querySelector('.form-control')
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  // sessionStorage.setItem('inputVal', input.value); 
+})
